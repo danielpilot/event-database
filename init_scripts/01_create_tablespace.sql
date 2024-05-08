018c0eaaf209fab33fc2a7428e06d43b5389cd3e
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-08 20:52:57 +0200

    Start adding tablespaces and startup logic

---
+++ init_scripts/01_create_tablespace.sql
@@ -0,0 +1,2 @@
+CREATE TABLESPACE operational_tablespace LOCATION '/var/lib/pg_tablespaces/data/operational_tablespace';
+CREATE TABLESPACE warehouse_tablespace LOCATION '/var/lib/pg_tablespaces/data/warehouse_tablespace';
