d1737e9b93f530807d125d620a572f0bd7aff6af
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-15 12:48:43 +0200

    Grant all privileges on tables

--- init_scripts/08_user_privileges.sql
+++ init_scripts/08_user_privileges.sql
@@ -1,5 +1,10 @@
 \c event_database;
 
 GRANT ALL PRIVILEGES ON SCHEMA events TO event_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA events TO event_user;
+
 GRANT ALL PRIVILEGES ON SCHEMA statistics TO statistics_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA statistics TO statistics_user;
+
 GRANT ALL PRIVILEGES ON SCHEMA logs TO log_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA logs TO log_user;
