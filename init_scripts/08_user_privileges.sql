3d8fb1586bd084f1cdb75317c5b84399b5adcd38
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-15 12:44:28 +0200

    Add user privileges

---
+++ init_scripts/08_user_privileges.sql
@@ -0,0 +1,5 @@
+\c event_database;
+
+GRANT ALL PRIVILEGES ON SCHEMA events TO event_user;
+GRANT ALL PRIVILEGES ON SCHEMA statistics TO statistics_user;
+GRANT ALL PRIVILEGES ON SCHEMA logs TO log_user;
