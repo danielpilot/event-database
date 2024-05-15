1c1bfd9ce4f28edc4f8397d6be2a6139ac6a0ed0
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-15 18:22:36 +0200

    Create location delete procedure

--- init_scripts/10_create_procedures.sql
+++ init_scripts/10_create_procedures.sql
@@ -127,3 +127,56 @@ AS
         END
     $$
 LANGUAGE plpgsql;
+
+-- Delete location
+CREATE FUNCTION events.delete_location(
+    _location_id events.location.id%type
+) RETURNS TEXT
+AS
+    $$
+        DECLARE
+            _location_exists  BOOLEAN;
+            _event_exists     BOOLEAN;
+            _entry_parameters VARCHAR(255);
+            _procedure_id     INTEGER;
+            _result           TEXT;
+        BEGIN
+            _entry_parameters := format('ID: %s', _location_id);
+
+            SELECT id INTO _procedure_id FROM logs.Procedures WHERE name = 'delete_location';
+
+            IF _procedure_id IS NULL THEN
+                RAISE EXCEPTION 'Procedure delete_location is not registered in the procedures table';
+            END IF;
+
+            SELECT EXISTS (SELECT 1 FROM events.location WHERE id = _location_id) INTO _location_exists;
+
+            IF NOT _location_exists THEN
+                RAISE EXCEPTION 'Location % does not exist', _location_id;
+            END IF;
+
+            SELECT EXISTS (SELECT 1 FROM events.event WHERE location_id = _location_id) INTO _event_exists;
+
+            IF _event_exists THEN
+                RAISE EXCEPTION 'Location % has related events', _location_id;
+            END IF;
+
+            DELETE FROM events.location WHERE id = _location_id;
+
+            _result := 'OK';
+
+            INSERT INTO logs.Log (date, db_user, procedure_id, entry_parameters, result)
+            VALUES (NOW(), CURRENT_USER, _procedure_id, _entry_parameters, _result);
+
+            RETURN _result;
+        EXCEPTION
+            WHEN raise_exception THEN
+                _result := format('ERROR: %s', SQLERRM);
+
+                INSERT INTO logs.Log (date, db_user, procedure_id, entry_parameters, result)
+                VALUES (NOW(), CURRENT_USER, _procedure_id, _entry_parameters, _result);
+
+                RETURN _result;
+        END
+    $$
+LANGUAGE plpgsql;
