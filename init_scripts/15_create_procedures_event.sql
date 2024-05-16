8f3070544090c85b90f4f59fefd841bb10488fe6
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-16 09:19:45 +0200

    Add event sales data

--- init_scripts/15_create_procedures_event.sql
+++ init_scripts/15_create_procedures_event.sql
@@ -1,5 +1,12 @@
 \c event_database;
 
+-- Create sales type to group sales parameters
+CREATE TYPE events.event_sales_data AS
+(
+    capacity         SMALLINT,
+    maximum_per_sale SMALLINT
+);
+
 -- Create event
 CREATE FUNCTION events.create_event(
     _name events.Event.name%type,
@@ -14,20 +21,32 @@ CREATE FUNCTION events.create_event(
     _comments events.Event.comments%type,
     _organizer_id events.Event.organizer_id%type,
     _location_id events.Event.location_id%type,
-    _categories INT[]
+    _categories INT[],
+    _event_sales_data events.event_sales_data DEFAULT NULL
 ) RETURNS TEXT AS
 $$
 DECLARE
-    _inserted_event_id      INT;
-    _entry_parameters       TEXT;
-    _procedure_id           INTEGER;
-    _result                 TEXT;
-    _valid_categories_count INTEGER;
-    _category_id            INTEGER;
+    _inserted_event_id          INT;
+    _entry_parameters           TEXT;
+    _procedure_id               INTEGER;
+    _result                     TEXT;
+    _valid_categories_count     INTEGER;
+    _category_id                INTEGER;
+    _formatted_event_sales_data TEXT;
 BEGIN
+    _formatted_event_sales_data := 'NULL';
+
+    IF _event_sales_data IS NOT NULL THEN
+        _formatted_event_sales_data := format(
+                'Capacity: %s - Maximum Per Sale: %s',
+                _event_sales_data.capacity,
+                _event_sales_data.maximum_per_sale);
+    END IF;
+
     _entry_parameters := format(
             'Name: %s | Start Date: %s | End Date: %s | Schedule: %s | Description: %s | Price: %s | Image: %s | ' ||
-            'Event Status: %s | Event Published: %s | Comments: %s | Organizer ID: %s | Location ID: %s | Categories: %s',
+            'Event Status: %s | Event Published: %s | Comments: %s | Organizer ID: %s | Location ID: %s' ||
+            ' | Categories: %s | Event Sales Data: %s',
             _name,
             _start_date,
             _end_date,
@@ -40,8 +59,8 @@ BEGIN
             _comments,
             _organizer_id,
             _location_id,
-            array_to_string(_categories::text[], ', ')
-                         );
+            array_to_string(_categories::text[], ', '),
+            _formatted_event_sales_data);
 
     BEGIN
         SELECT id INTO _procedure_id FROM logs.Procedures WHERE name = 'create_event';
@@ -94,6 +113,10 @@ BEGIN
                 VALUES (_inserted_event_id, _category_id);
             END LOOP;
 
+        IF _event_sales_data IS NOT NULL THEN
+            INSERT INTO events.Event_With_Sales (event_id, capacity, maximum_per_sale)
+            VALUES (_inserted_event_id, _event_sales_data.capacity, _event_sales_data.capacity);
+        END IF;
 
         _result := 'OK';
     EXCEPTION
@@ -123,16 +146,25 @@ CREATE FUNCTION events.update_event(
     _comments events.Event.comments%type,
     _organizer_id events.Event.organizer_id%type,
     _location_id events.Event.location_id%type,
-    _categories INT[]
+    _categories INT[],
+    _event_sales_data events.event_sales_data DEFAULT NULL
 ) RETURNS TEXT AS
 $$
 DECLARE
-    _entry_parameters       TEXT;
-    _procedure_id           INTEGER;
-    _result                 TEXT;
-    _valid_categories_count INTEGER;
-    _category_id            INTEGER;
+    _entry_parameters           TEXT;
+    _procedure_id               INTEGER;
+    _result                     TEXT;
+    _valid_categories_count     INTEGER;
+    _category_id                INTEGER;
+    _formatted_event_sales_data TEXT;
 BEGIN
+    IF _event_sales_data IS NOT NULL THEN
+        _formatted_event_sales_data := format(
+                'Capacity: %s - Maximum Per Sale: %s',
+                _event_sales_data.capacity,
+                _event_sales_data.maximum_per_sale);
+    END IF;
+
     _entry_parameters := format(
             'ID: %s | Name: %s | Start Date: %s | End Date: %s | Schedule: %s | Description: %s | Price: %s' ||
             ' | Image: %s | Event Status: %s | Event Published: %s | Comments: %s | Organizer ID: %s' ||
@@ -150,8 +182,8 @@ BEGIN
             _comments,
             _organizer_id,
             _location_id,
-            array_to_string(_categories::text[], ', ')
-                         );
+            array_to_string(_categories::text[], ', '),
+            _formatted_event_sales_data);
 
     BEGIN
         SELECT id INTO _procedure_id FROM logs.Procedures WHERE name = 'update_event';
@@ -200,8 +232,25 @@ BEGIN
                 VALUES (_id, _category_id);
             END LOOP;
 
+        -- Handle sales data
+        IF _event_sales_data IS NOT NULL THEN
+            IF EXISTS (SELECT 1 FROM events.Event_With_Sales WHERE event_id = _id) THEN
+                UPDATE events.Event_With_Sales
+                SET capacity         = _event_sales_data.capacity,
+                    maximum_per_sale = _event_sales_data.maximum_per_sale
+                WHERE event_id = _id;
+            ELSE
+                INSERT INTO events.Event_With_Sales (event_id, capacity, maximum_per_sale)
+                VALUES (_id, _event_sales_data.capacity, _event_sales_data.maximum_per_sale);
+            END IF;
+        ELSE
+            DELETE FROM events.Event_With_Sales WHERE event_id = _id;
+        END IF;
+
         _result := 'OK';
     EXCEPTION
+        WHEN foreign_key_violation THEN
+            _result := format('ERROR: Unable to remove event with sales with related transactions');
         WHEN raise_exception THEN
             _result := format('ERROR: %s', SQLERRM);
     END;
