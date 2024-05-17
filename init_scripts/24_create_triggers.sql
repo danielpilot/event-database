1dbbd6f8626443917979f57647373ba9b691fb84
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-17 18:06:38 +0200

    Create check transaction conditions before insert

---
+++ init_scripts/24_create_triggers.sql
@@ -0,0 +1,38 @@
+\c event_database;
+
+-- Check event capacity before insert
+CREATE FUNCTION check_transaction_conditions_before_insert() RETURNS trigger AS
+$$
+DECLARE
+    _capacity           SMALLINT;
+    _sales              SMALLINT;
+    _event_id           INTEGER;
+    _available_capacity INTEGER;
+    _event_has_sales    BOOLEAN;
+BEGIN
+    SELECT (ews.capacity, ews.sales, ews.event_id)
+    INTO _capacity, _sales, _event_id
+    FROM events.Event_With_Sales ews
+    WHERE id = NEW.event_id;
+
+    _available_capacity := _capacity - _sales;
+
+    IF NEW.quantity > _available_capacity THEN
+        RAISE EXCEPTION 'Not enough tickets are available';
+    END IF;
+
+    SELECT e.event_has_sales INTO _event_has_sales FROM events.Event e WHERE id = _event_id;
+
+    IF NOT _event_has_sales THEN
+        RAISE EXCEPTION 'Event sales are closed';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_check_transaction_conditions_before_insert
+    BEFORE INSERT
+    ON events.transaction
+    FOR EACH ROW
+EXECUTE PROCEDURE check_transaction_conditions_before_insert();
