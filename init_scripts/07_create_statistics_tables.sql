e05d71ca5fa9942d2f3c6fdc133a135c190177ca
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-21 17:37:49 +0200

    Add missing top commented events table

--- init_scripts/07_create_statistics_tables.sql
+++ init_scripts/07_create_statistics_tables.sql
@@ -1,80 +1,99 @@
 \c event_database;
 
 -- Create city statistics table
-CREATE TABLE statistics.City_Statistics (
+CREATE TABLE statistics.City_Statistics
+(
     city_id INTEGER PRIMARY KEY,
-    events INTEGER NOT NULL
+    events  INTEGER NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create location statistics table
-CREATE TABLE statistics.Location_Statistics (
+CREATE TABLE statistics.Location_Statistics
+(
     location_id INTEGER PRIMARY KEY,
-    events INTEGER
+    events      INTEGER
 ) TABLESPACE warehouse_tablespace;
 
 -- Create event statistics table
-CREATE TABLE statistics.Event_Statistics (
-    event_id INTEGER PRIMARY KEY,
-    comments INTEGER NOT NULL,
-    average_rating REAL NOT NULL,
-    sales INTEGER NOT NULL,
-    occupancy REAL NOT NULL
+CREATE TABLE statistics.Event_Statistics
+(
+    event_id       INTEGER PRIMARY KEY,
+    comments       INTEGER NOT NULL DEFAULT 0,
+    average_rating REAL    NOT NULL DEFAULT 0.0,
+    sales          INTEGER NOT NULL DEFAULT 0,
+    occupancy      REAL    NOT NULL DEFAULT 0.0
 ) TABLESPACE warehouse_tablespace;
 
 -- Create transaction statistics table
-CREATE TABLE statistics.Transaction_Statistics (
-    month SMALLINT NOT NULL,
-    year SMALLINT NOT NULL,
-    transactions INTEGER NOT NULL,
+CREATE TABLE statistics.Transaction_Statistics
+(
+    month        SMALLINT NOT NULL,
+    year         SMALLINT NOT NULL,
+    transactions INTEGER  NOT NULL,
     PRIMARY KEY (month, year)
 ) TABLESPACE warehouse_tablespace;
 
 -- Create event with sale statistics table
-CREATE TABLE statistics.Event_With_Sale_Statistics (
-    event_id INTEGER PRIMARY KEY,
-    price REAL NOT NULL,
-    sells INTEGER NOT NULL,
-    occupancy REAL NOT NULL
+CREATE TABLE statistics.Event_With_Sale_Statistics
+(
+    event_id  INTEGER PRIMARY KEY,
+    price     REAL    NOT NULL,
+    sells     INTEGER NOT NULL,
+    occupancy REAL    NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create system counters table
-CREATE TABLE statistics.System_Counters (
-    name VARCHAR(30) PRIMARY KEY,
+CREATE TABLE statistics.System_Counters
+(
+    name  VARCHAR(30) PRIMARY KEY,
     value INTEGER NOT NULL
+) TABLESPACE warehouse_tablespace;
+
+-- Create top commented events table
+CREATE TABLE statistics.Top_Commented_Events
+(
+    event_id INTEGER PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create top valued events table
-CREATE TABLE statistics.Top_Valued_Events (
+CREATE TABLE statistics.Top_Valued_Events
+(
     event_id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name     VARCHAR(255) NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create top sold events table
-CREATE TABLE statistics.Top_Sold_Events (
+CREATE TABLE statistics.Top_Sold_Events
+(
     event_id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name     VARCHAR(255) NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create top event locations table
-CREATE TABLE statistics.Top_Event_Locations (
+CREATE TABLE statistics.Top_Event_Locations
+(
     event_id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name     VARCHAR(255) NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create top favorite events table
-CREATE TABLE statistics.Top_Favorite_Events (
+CREATE TABLE statistics.Top_Favorite_Events
+(
     event_id INTEGER PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name     VARCHAR(255) NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create integer indicators table
-CREATE TABLE statistics.Integer_Indicators (
+CREATE TABLE statistics.Integer_Indicators
+(
     indicator SMALLINT PRIMARY KEY CHECK ( indicator IN ('1') ),
-    value INTEGER NOT NULL
+    value     INTEGER NOT NULL
 ) TABLESPACE warehouse_tablespace;
 
 -- Create percentage indicators table
-CREATE TABLE statistics.Percentage_Indicators (
+CREATE TABLE statistics.Percentage_Indicators
+(
     indicator SMALLINT PRIMARY KEY CHECK ( indicator in ('1', '2', '3', '4', '5', '6', '7')),
-    value REAL NOT NULL
+    value     REAL NOT NULL
 ) TABLESPACE warehouse_tablespace;
