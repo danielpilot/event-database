55b3a79edeaaa90327717b5e08eccd93090b7801
Daniel Bertrand Pilot Combarro <dbertrand@ces.vocento.com>
2024-05-26 13:52:36 +0200

    Start creating event view

---
+++ init_scripts/27_create_statistics_views.sql
@@ -0,0 +1,8 @@
+\c event_database;
+
+-- Create Top 10 most commented events view
+CREATE VIEW statistics.Top10EventsWithMostViews AS
+SELECT event_id, comments
+FROM statistics.event_statistics
+ORDER BY comments DESC
+LIMIT 10;
