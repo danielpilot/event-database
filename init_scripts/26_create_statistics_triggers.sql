577e2575e97b9b71e1b49528403fe35282de09b2
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-27 17:40:06 +0200

    Modify statistics views and create delete trigger

--- init_scripts/26_create_statistics_triggers.sql
+++ init_scripts/26_create_statistics_triggers.sql
@@ -1,5 +1,6 @@
 \c event_database;
 
+-- Update event statistics on rating insert
 CREATE FUNCTION events.update_event_statistics_on_rating_insert() RETURNS TRIGGER AS
 $$
 BEGIN
@@ -11,12 +12,33 @@ BEGIN
         INSERT INTO statistics.event_statistics (event_id, comments, average_rating, sales, occupancy)
         VALUES (NEW.event_id, 1, 0, 0, 0);
     END IF;
+
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
 
 CREATE TRIGGER trg_update_event_statistics_on_rating_insert
     BEFORE INSERT
     ON events.rating
     FOR EACH ROW
 EXECUTE PROCEDURE events.update_event_statistics_on_rating_insert();
+
+-- Update event statistics on rating delete
+CREATE FUNCTION events.update_event_statistics_on_rating_delete() RETURNS TRIGGER AS
+$$
+BEGIN
+    IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = OLD.event_id) THEN
+        UPDATE statistics.event_statistics
+        SET comments = comments - 1
+        WHERE event_id = NEW.event_id;
+    END IF;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_event_statistics_on_rating_insert
+    AFTER DELETE
+    ON events.rating
+    FOR EACH ROW
+EXECUTE PROCEDURE events.update_event_statistics_on_rating_delete();
--- init_scripts/27_create_statistics_views.sql
+++ init_scripts/27_create_statistics_views.sql
@@ -1,7 +1,7 @@
 \c event_database;
 
 -- Create Top 10 most commented events view
-CREATE VIEW statistics.Top10EventsWithMostViews AS
+CREATE VIEW statistics.Top10EventsWithMostComments AS
 SELECT event_id, comments
 FROM statistics.event_statistics
 ORDER BY comments DESC
