f26c062aad8d27481415d41f8436a864d722befe
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-27 18:29:58 +0200

    Modify statistics triggers to take into account the rating publication status

--- init_scripts/26_create_statistics_triggers.sql
+++ init_scripts/26_create_statistics_triggers.sql
@@ -4,6 +4,11 @@
 CREATE FUNCTION events.update_event_statistics_on_rating_insert() RETURNS TRIGGER AS
 $$
 BEGIN
+    -- We only modify the statistics if the event is published
+    IF NOT NEW.published THEN
+        RETURN NEW;
+    END IF;
+
     IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = NEW.event_id) THEN
         UPDATE statistics.event_statistics
         SET ratings_count  = ratings_count + 1,
@@ -34,6 +39,42 @@ EXECUTE PROCEDURE events.update_event_statistics_on_rating_insert();
 CREATE FUNCTION events.update_event_statistics_on_rating_update() RETURNS TRIGGER AS
 $$
 BEGIN
+    -- We only modify the statistics if the event is published
+    IF NOT OLD.published AND NOT NEW.published THEN
+        RETURN NEW;
+    END IF;
+
+    -- If the event was not published and now it is, we insert the rating
+    IF NOT OLD.published AND NEW.published THEN
+        IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = NEW.event_id) THEN
+            UPDATE statistics.event_statistics
+            SET ratings_count  = ratings_count + 1,
+                average_rating = (total_rating + NEW.punctuation::float) / (ratings_count + 1),
+                total_rating   = total_rating + NEW.punctuation
+            WHERE event_id = NEW.event_id;
+        ELSE
+            INSERT INTO statistics.event_statistics (event_id,
+                                                     ratings_count,
+                                                     average_rating,
+                                                     total_rating,
+                                                     sales,
+                                                     occupancy)
+            VALUES (NEW.event_id, 1, NEW.punctuation, NEW.punctuation, 0, 0);
+        END IF;
+    END IF;
+
+    -- If the event is not published anymore, we delete the rating
+    IF OLD.published AND NOT NEW.published THEN
+        IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = OLD.event_id) THEN
+            UPDATE statistics.event_statistics
+            SET ratings_count  = ratings_count - 1,
+                average_rating = (total_rating - OLD.punctuation::float) / (ratings_count - 1),
+                total_rating   = total_rating - OLD.punctuation
+            WHERE event_id = NEW.event_id;
+        END IF;
+    END IF;
+
+    -- If the event was published and it's still published, we update the rating
     IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = NEW.event_id) THEN
         UPDATE statistics.event_statistics
         SET average_rating = (total_rating + NEW.punctuation::float - OLD.punctuation::float) / ratings_count,
@@ -55,6 +96,10 @@ EXECUTE PROCEDURE events.update_event_statistics_on_rating_update();
 CREATE FUNCTION events.update_event_statistics_on_rating_delete() RETURNS TRIGGER AS
 $$
 BEGIN
+    IF NOT OLD.published THEN
+        RETURN OLD;
+    END IF;
+
     IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = OLD.event_id) THEN
         UPDATE statistics.event_statistics
         SET ratings_count  = ratings_count - 1,
