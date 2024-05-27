92e066f846b8a60b095a516c25e127562367c65d
Daniel Pilot <daniel.pilot.combarro@gmail.com>
2024-05-27 18:13:13 +0200

    Add function for most valued events

--- init_scripts/26_create_statistics_triggers.sql
+++ init_scripts/26_create_statistics_triggers.sql
@@ -6,11 +6,18 @@ $$
 BEGIN
     IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = NEW.event_id) THEN
         UPDATE statistics.event_statistics
-        SET ratings_count = ratings_count + 1
+        SET ratings_count  = ratings_count + 1,
+            average_rating = (total_rating + NEW.punctuation::float) / (ratings_count + 1),
+            total_rating   = total_rating + NEW.punctuation
         WHERE event_id = NEW.event_id;
     ELSE
-        INSERT INTO statistics.event_statistics (event_id, ratings_count, average_rating, sales, occupancy)
-        VALUES (NEW.event_id, 1, 0, 0, 0);
+        INSERT INTO statistics.event_statistics (event_id,
+                                                 ratings_count,
+                                                 average_rating,
+                                                 total_rating,
+                                                 sales,
+                                                 occupancy)
+        VALUES (NEW.event_id, 1, NEW.punctuation, NEW.punctuation, 0, 0);
     END IF;
 
     RETURN NEW;
@@ -23,13 +30,36 @@ CREATE TRIGGER trg_update_event_statistics_on_rating_insert
     FOR EACH ROW
 EXECUTE PROCEDURE events.update_event_statistics_on_rating_insert();
 
+-- Update event statistics on rating update
+CREATE FUNCTION events.update_event_statistics_on_rating_update() RETURNS TRIGGER AS
+$$
+BEGIN
+    IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = NEW.event_id) THEN
+        UPDATE statistics.event_statistics
+        SET average_rating = (total_rating + NEW.punctuation::float - OLD.punctuation::float) / ratings_count,
+            total_rating   = total_rating + NEW.punctuation - OLD.punctuation
+        WHERE event_id = NEW.event_id;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_event_statistics_on_rating_update
+    AFTER UPDATE
+    ON events.rating
+    FOR EACH ROW
+EXECUTE PROCEDURE events.update_event_statistics_on_rating_update();
+
 -- Update event statistics on rating delete
 CREATE FUNCTION events.update_event_statistics_on_rating_delete() RETURNS TRIGGER AS
 $$
 BEGIN
     IF EXISTS (SELECT event_id FROM statistics.event_statistics WHERE rating.event_id = OLD.event_id) THEN
         UPDATE statistics.event_statistics
-        SET ratings_count = ratings_count - 1
+        SET ratings_count  = ratings_count - 1,
+            average_rating = (total_rating - OLD.punctuation::float) / (ratings_count - 1),
+            total_rating   = total_rating - OLD.punctuation
         WHERE event_id = NEW.event_id;
     END IF;
 
--- init_scripts/27_create_statistics_views.sql
+++ init_scripts/27_create_statistics_views.sql
@@ -6,3 +6,10 @@ SELECT event_id, ratings_count
 FROM statistics.event_statistics
 ORDER BY ratings_count DESC
 LIMIT 10;
+
+-- Create Top 10 best valued events view
+CREATE VIEW statistics.Top10EventsWithBestPunctuation AS
+SELECT event_id, average_rating, ratings_count
+FROM statistics.event_statistics
+ORDER BY average_rating DESC
+LIMIT 10;
